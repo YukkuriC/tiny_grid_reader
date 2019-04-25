736c5654b09c5be7a18b368ab84b8141e9fb3bdd
YukkuriC <34098703+YukkuriC@users.noreply.github.com>
2019-04-25 21:10:19 +0800

    自动对网格进行排序

--- reader.py
+++ reader.py
@@ -2,7 +2,7 @@ from math import floor, ceil
 
 
 class Mapper:
-    def __init__(self, filename, sep=',', flipx=False, flipy=False):
+    def __init__(self, filename, sep=','):
         with open(filename) as f:
             data = f.read().split('\n')
         self.ylab = []
@@ -10,19 +10,20 @@ class Mapper:
         for i, line in enumerate(data):
             if i == 0:
                 self.xlab = list(map(float, line.split(sep)[1:]))
-                if flipx:
-                    self.xlab = self.xlab[::-1]
+                x_sorter = sorted(
+                    range(len(self.xlab)), key=self.xlab.__getitem__)
+                self.xlab = list(map(self.xlab.__getitem__, x_sorter))
+                print(self.xlab)
                 continue
             if not line:
                 continue
             line = list(map(float, line.split(sep)))
             self.ylab.append(line.pop(0))
-            if flipx:
-                line = linex[::-1]
+            line = list(map(line.__getitem__, x_sorter))
             self.data.append(line)
-        if flipy:
-            self.data = self.data[::-1]
-            self.ylab = self.ylab[::-1]
+        y_sorter = sorted(range(len(self.ylab)), key=self.ylab.__getitem__)
+        self.ylab = list(map(self.ylab.__getitem__, y_sorter))
+        self.data = list(map(self.data.__getitem__, y_sorter))
 
     @staticmethod
     def bsearch(lst, val):
@@ -70,7 +71,7 @@ print('Reader'.center(30, '='))
 if __name__ == '__main__':
     import sys
     PATH = r"""test.csv"""
-    test = Mapper(PATH, ',', flipy=1)
+    test = Mapper(PATH, ',')
     while 1:
         try:
             row, col = map(float, input('输入坐标，空格分隔: ').split())
